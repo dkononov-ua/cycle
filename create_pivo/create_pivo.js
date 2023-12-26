3c16b1a482b8fd06348a1ebe810cf75a91d758d9
Denys <dkononov.ua@gmail.com>
2023-12-26 11:09:21 +0200

    2

--- create_pivo/create_pivo.js
+++ create_pivo/create_pivo.js
@@ -1,5 +1,5 @@
 async function sss(){
-	await fetch('https://backend.discussio.space:8000/img/users/user_defaulttttt.svg')
+	await fetch('https://sky.syrykh.com:8000/img/users/user_defaulttttt.svg')
 	console.log(123123)
 	setTimeout(sss, 7200000)
 }
